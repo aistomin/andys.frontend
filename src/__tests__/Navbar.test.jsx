f67ac53626eea17dbe206d9837891c7546078ea0
aistomin <andrej.istomin.ikeen@gmail.com>
2023-06-17 03:41:51 +0200

    Issue #10. Navbar.test.jsx code reformatted.

--- src/__tests__/Navbar.test.jsx
+++ src/__tests__/Navbar.test.jsx
@@ -7,16 +7,22 @@ window.scrollTo = jest.fn();
 describe("Navbar Component", function () {
 
     it("should have correct logo", function () {
-        const { getByTestId } = render(<App />);
-        expect(getByTestId("logo")).toHaveTextContent("ANDY GRAILS");
+        const {getByTestId} = render(<App/>);
+        expect(getByTestId("logo"))
+            .toHaveTextContent("ANDY GRAILS");
     });
 
     it("should have correct navigation links", function () {
-        const { getByRole } = render(<App />);
-        expect(getByRole('link', {name: 'Home'})).toHaveAttribute('href', '/');
-        expect(getByRole('link', {name: 'Videos'})).toHaveAttribute('href', '/videos');
-        expect(getByRole('link', {name: 'Notes/Tabs'})).toHaveAttribute('href', '/notes');
-        expect(getByRole('link', {name: 'Lyrics'})).toHaveAttribute('href', '/lyrics');
-        expect(getByRole('link', {name: 'About'})).toHaveAttribute('href', '/about');
+        const {getByRole} = render(<App/>);
+        expect(getByRole('link', {name: 'Home'}))
+            .toHaveAttribute('href', '/');
+        expect(getByRole('link', {name: 'Videos'}))
+            .toHaveAttribute('href', '/videos');
+        expect(getByRole('link', {name: 'Notes/Tabs'}))
+            .toHaveAttribute('href', '/notes');
+        expect(getByRole('link', {name: 'Lyrics'}))
+            .toHaveAttribute('href', '/lyrics');
+        expect(getByRole('link', {name: 'About'}))
+            .toHaveAttribute('href', '/about');
     });
 });
