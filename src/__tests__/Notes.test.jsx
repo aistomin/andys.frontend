c366ecd6d3b06ae397f60b857077615e2bcfa3c4
aistomin <andrej.istomin.ikeen@gmail.com>
2023-06-29 09:24:42 +0200

    Issue #70. Notes.jsx is covered with tests.

---
+++ src/__tests__/Notes.test.jsx
@@ -0,0 +1,21 @@
+import {renderAppWithStore} from "../__setup__/setupTests.jsx";
+import {fireEvent} from "@testing-library/react";
+
+describe("Notes Component", () => {
+
+    it("should have a proper title", () => {
+        const {container, getByRole} = renderAppWithStore();
+        const videosMenu = getByRole('link', {name: 'Notes/Tabs'});
+        fireEvent.click(videosMenu);
+        const title = container.getElementsByClassName("music-sheets-heading")[0];
+        expect(title.textContent).toBe("My latest Music Sheets")
+    });
+
+    it("should display all the existing videos", () => {
+        const {container, getByRole} = renderAppWithStore();
+        const videosMenu = getByRole('link', {name: 'Notes/Tabs'});
+        fireEvent.click(videosMenu);
+        const cards = container.getElementsByClassName("item-card");
+        expect(cards.length).toBe(2);
+    });
+});
