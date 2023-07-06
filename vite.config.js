f113baa6fdc3a47099ca1cb34695b63d99e54d5f
aistomin <andrej.istomin.ikeen@gmail.com>
2023-07-06 13:40:59 +0200

    Issue #115. sitemap.xml is added manually. the shitty generators are removed.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import generateSitemap from "vite-plugin-pages-sitemap";
-import Pages from 'vite-plugin-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-    Pages({
-      onRoutesGenerated: routes => (generateSitemap({ routes })),
-    })
-  ],
+    plugins: [
+        react()
+    ]
 });
